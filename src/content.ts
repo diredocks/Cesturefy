251108b24f1469d18ac57b8c1e27a9754d746511
diredocks <26994007+diredocks@users.noreply.github.com>
2025-09-22 16:17:03 +0800

    feat: add pattern

--- src/content.ts
+++ src/content.ts
@@ -1,5 +1,9 @@
 import MouseController from "@controller/mouse";
 import TraceView from "@views/trace/trace";
+import Pattern from "@utils/pattern";
+import { Message, BackgroundMessages } from "@utils/messaging";
+
+const pattern = new Pattern(0.12, 10);
 
 main();
 
@@ -31,9 +35,25 @@ MouseController.emitter.addEventListener('update', (_es, e) => {
 
 MouseController.emitter.addEventListener('end', (_es, _e) => {
   TraceView.terminate();
+  sendBackgroundMessage('gestureEnd', pattern.getPattern());
 });
 
 function mouseGestureUpdate(es: (PointerEvent)[]) {
+  for (const e of es) {
+    const patternChange = pattern.addPoint(e.clientX, e.clientY);
+    if (patternChange) {
+      sendBackgroundMessage('gestureChange', pattern.getPattern());
+    }
+  }
+
   const points = es.map(e => ({ x: e.clientX, y: e.clientY }));
   TraceView.updateTrace(points);
-} 
+}
+
+function sendBackgroundMessage<K extends keyof BackgroundMessages>(
+  subject: K,
+  data: BackgroundMessages[K]
+) {
+  const msg: Message<K, BackgroundMessages> = { subject, data };
+  return chrome.runtime.sendMessage(msg);
+}
---
+++ src/utils/pattern.ts
@@ -0,0 +1,95 @@
+// TODO: also figure out how this works
+
+import { vectorDirectionDifference } from "@utils/common";
+import { Vector } from "@utils/types";
+
+export enum PatternStatus {
+  PASSED_NO_THRESHOLD = 0,
+  PASSED_DISTANCE_THRESHOLD = 1,
+  PASSED_DIFFERENCE_THRESHOLD = 2,
+}
+
+export default class Pattern {
+  private differenceThreshold: number;
+  private distanceThreshold: number;
+
+  private lastExtractedPoint: Vector | null = null;
+  private previousPoint: Vector | null = null;
+  private lastPoint: Vector | null = null;
+  private previousVector: Vector | null = null;
+
+  private extractedVectors: Vector[] = [];
+
+  constructor(differenceThreshold = 0, distanceThreshold = 0) {
+    this.differenceThreshold = differenceThreshold;
+    this.distanceThreshold = distanceThreshold;
+  }
+
+  clear(): void {
+    this.extractedVectors = [];
+    this.lastExtractedPoint = null;
+    this.previousPoint = null;
+    this.lastPoint = null;
+    this.previousVector = null;
+  }
+
+  addPoint(x: number, y: number): PatternStatus {
+    const point: Vector = [x, y];
+    let status: PatternStatus = PatternStatus.PASSED_NO_THRESHOLD;
+
+    if (!this.previousPoint) {
+      this.previousPoint = point;
+      this.lastExtractedPoint = point;
+      this.lastPoint = point;
+      return status;
+    }
+
+    const [prevX, prevY] = this.previousPoint;
+    const newVector: Vector = [x - prevX, y - prevY];
+    const distance = Math.hypot(...newVector);
+
+    if (distance <= this.distanceThreshold) {
+      this.lastPoint = point;
+      return status;
+    }
+
+    if (!this.previousVector) {
+      this.previousVector = newVector;
+      status = PatternStatus.PASSED_DISTANCE_THRESHOLD;
+    } else {
+      const diff = vectorDirectionDifference(
+        this.previousVector[0],
+        this.previousVector[1],
+        newVector[0],
+        newVector[1]
+      );
+
+      if (Math.abs(diff) > this.differenceThreshold && this.lastExtractedPoint) {
+        this.extractedVectors.push([
+          prevX - this.lastExtractedPoint[0],
+          prevY - this.lastExtractedPoint[1],
+        ]);
+        this.previousVector = newVector;
+        this.lastExtractedPoint = this.previousPoint;
+        status = PatternStatus.PASSED_DIFFERENCE_THRESHOLD;
+      } else {
+        status = PatternStatus.PASSED_DISTANCE_THRESHOLD;
+      }
+    }
+
+    this.previousPoint = point;
+    this.lastPoint = point;
+    return status;
+  }
+
+  getPattern(): Vector[] {
+    if (!this.lastPoint || !this.lastExtractedPoint) return [];
+
+    const lastVector: Vector = [
+      this.lastPoint[0] - this.lastExtractedPoint[0],
+      this.lastPoint[1] - this.lastExtractedPoint[1],
+    ];
+
+    return [...this.extractedVectors, lastVector];
+  }
+}
