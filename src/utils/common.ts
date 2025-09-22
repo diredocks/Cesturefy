1688dda9e49cd3ec5c8e85ced7d4dc12c3926139
diredocks <26994007+diredocks@users.noreply.github.com>
2025-09-22 16:17:03 +0800

    feat: add pattern

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -8,3 +8,18 @@ export function preventDefault(e: Event) {
 export function getDistance(x1: number, y1: number, x2: number, y2: number) {
   return Math.hypot(x2 - x1, y2 - y1);
 }
+
+export function vectorDirectionDifference(
+  V1X: number, V1Y: number,
+  V2X: number, V2Y: number
+): number {
+  let angleDifference = Math.atan2(V1X, V1Y) - Math.atan2(V2X, V2Y);
+
+  if (angleDifference > Math.PI) {
+    angleDifference -= 2 * Math.PI;
+  } else if (angleDifference <= -Math.PI) {
+    angleDifference += 2 * Math.PI;
+  }
+
+  return angleDifference / Math.PI;
+}
