af2cf4f6c96f7240becd11cd166e41059d0084d7
diredocks <26994007+diredocks@users.noreply.github.com>
2025-09-23 15:00:58 +0800

    feat: init commands

---
+++ src/commands/duplicate-tab.ts
@@ -0,0 +1,39 @@
+import type { CommandFn } from "@utils/types";
+
+export interface DuplicateTabSettings {
+  focusPrevTab?: boolean;
+  position?: 'before' | 'after' | 'start' | 'end';
+}
+
+export const DuplicateTab: CommandFn<DuplicateTabSettings> = async function (sender) {
+  if (!sender.tab?.id) {
+    return true;
+  }
+
+  const duplicated = await chrome.tabs.duplicate(sender.tab.id);
+  if (!duplicated?.id) {
+    return true;
+  }
+
+  chrome.tabs.update(sender.tab.id, { active: this.getSetting('focusPrevTab') });
+
+  let index;
+  switch (this.getSetting('position')) {
+    case 'before':
+      index = sender.tab.id;
+      break;
+    case 'after':
+      index = sender.tab.index + 1;
+      break;
+    case 'start':
+      index = 0;
+      break;
+    case 'end':
+    default:
+      index = -1;
+      break;
+  }
+  await chrome.tabs.move(duplicated.id, { index });
+
+  return true;
+};
