cd256952b7051d701f23a63238d246e8a751f91c
diredocks <26994007+diredocks@users.noreply.github.com>
2025-09-27 11:28:50 +0800

    refactor(utils/match): use internal default

--- src/core/background.ts
+++ src/core/background.ts
@@ -1,7 +1,6 @@
 import Gesture, { GestureJSON } from "@model/gesture";
-import { DefaultConfig } from "@utils/config";
 import { configManager } from "@utils/config-manager";
-import { getGestureByPattern, MatchingAlgorithm } from "@utils/match";
+import { matcher } from "@utils/match";
 import {
   Handler, HandlerMap,
   registerHandlers, sendTabMessage,
@@ -9,21 +8,16 @@ import {
 } from "@utils/message";
 
 let gestures: Gesture[];
-// since pattern doesn't have a property to store
-let tolerance: number = DefaultConfig.Settings.Gesture.deviationTolerance;
-let algorithm: MatchingAlgorithm = DefaultConfig.Settings.Gesture.matchingAlgorithm;
 
 const applyGestures = () => {
   gestures = (configManager.getPath(['Gestures']) as GestureJSON[]).map(g => new Gesture(g));
-  tolerance = configManager.getPath(['Settings', 'Gesture', 'deviationTolerance']);
-  algorithm = configManager.getPath(['Settings', 'Gesture', 'matchingAlgorithm']);
 };
 
 configManager.addEventListener('loaded', applyGestures);
 configManager.addEventListener('change', applyGestures);
 
 const handleGestureChange: Handler<"gestureChange", BackgroundMessages> = (m, sender) => {
-  const matchedGesture = getGestureByPattern(m.data, gestures, tolerance, algorithm);
+  const matchedGesture = matcher.getGestureByPattern(m.data, gestures);
   if (!sender.tab?.id) {
     return;
   }
@@ -35,7 +29,7 @@ const handleGestureChange: Handler<"gestureChange", BackgroundMessages> = (m, se
 };
 
 const handleGestureEnd: Handler<"gestureEnd", BackgroundMessages> = (m, sender) => {
-  const matchedGesture = getGestureByPattern(m.data, gestures, tolerance, algorithm);
+  const matchedGesture = matcher.getGestureByPattern(m.data, gestures);
   if (matchedGesture) {
     matchedGesture.getCommand().execute(sender);
   }
