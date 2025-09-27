eca91d13d7d68896244f98c6804a821193339ada
diredocks <26994007+diredocks@users.noreply.github.com>
2025-09-27 11:58:11 +0800

    feat(options/about): support backup and reset

--- src/options/fragments/about.ts
+++ src/options/fragments/about.ts
@@ -1,21 +1,34 @@
 import { PopupBox } from "@options/components/popup-box";
 import { ContentLoaded } from "@options/index";
-// import { fetchJSONAsObject } from "@options/utils/common";
-// TODO: when config system ready
+import { configManager } from "@utils/config-manager";
 
 ContentLoaded.then(main);
 
 function main() {
   const resetButton = document.getElementById("resetButton")!;
   resetButton.onclick = onResetButton;
+  const backupButton = document.getElementById("backupButton")!;
+  backupButton.onclick = onBackupButton;
 }
 
 function onResetButton() {
   const popup = document.getElementById("resetConfirm")! as PopupBox;
   popup.addEventListener("close", (event) => {
-    if (event.detail) {
-      window.location.reload();
-    }
+    if (!event.detail) return;
+    configManager.clear().then(window.location.reload);
   }, { once: true });
   popup.open = true;
+}
+
+function onBackupButton() {
+  const manifest = chrome.runtime.getManifest();
+  const linkElement = document.createElement("a");
+  linkElement.download = `${manifest.name} ${manifest.version} ${new Date().toDateString()}.json`;
+  // creates a json file with the current config
+  linkElement.href = URL.createObjectURL(
+    new Blob([JSON.stringify(configManager.toJSON(), null, '  ')], { type: 'application/json' })
+  );
+  document.body.appendChild(linkElement);
+  linkElement.click();
+  document.body.removeChild(linkElement);
 }
